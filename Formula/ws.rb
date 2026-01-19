734f1189281e0f1bfca801708558e7ba2ff5aaa0
Theophile Cousin <cousin.theophile@gmail.com>
2026-01-19 12:13:07 +0100

    Update ws to v0.3.9

--- Formula/ws.rb
+++ Formula/ws.rb
@@ -1,24 +1,24 @@
 class Ws < Formula
   desc "Workspace CLI for git worktrees with tmux layouts"
   homepage "https://github.com/0xthc/ws-tools"
-  version "0.3.8"
+  version "0.3.9"
   license "MIT"
 
   on_macos do
     on_arm do
       url "https://github.com/0xthc/ws-tools/releases/download/v#{version}/ws-macos-aarch64.tar.gz"
-      sha256 "02adbd158e4a2f31e40c251c706f5b682b65f871c583933860a2e315c63d4b04"
+      sha256 "c2840bb0f9f9847031b539f773c423cef88270718f9793ef355e25d916c38e74"
     end
     on_intel do
       url "https://github.com/0xthc/ws-tools/releases/download/v#{version}/ws-macos-x86_64.tar.gz"
-      sha256 "719218aafd476a2dcfffd3ae8cfb47254c77253110ced13a66923f2fa9ca7f54"
+      sha256 "bc95cc12714c35fafe47bd4d0dce42f9918b319194f856803c4882a5bc3c0b64"
     end
   end
 
   on_linux do
     on_intel do
       url "https://github.com/0xthc/ws-tools/releases/download/v#{version}/ws-linux-x86_64.tar.gz"
-      sha256 "a962e7d75b0cc78b13b359fe6359e23502ca6fbade886e50df950e588b73d5bb"
+      sha256 "fb9ffb3a9476b4924322d336a681af24a456f7b871be960b948e706bf1242932"
     end
   end
 
